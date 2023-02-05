2f2590ed429245a2a03010d27a5ba08c76c63eda
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2023-02-05 22:08:21 +0100

    ThemeProvider working from styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { HelloWorld } from './components'
+import { LIGHT_THEME, DARK_THEME } from './themes'
 
 const App: React.FC = () => {
-  return <h1>Hello world</h1>
+  const [lightTheme, setLightTheme] = useState<boolean>(false)
+
+  const switchThemeHandler = () => {
+    setLightTheme(prevState => !prevState)
+  }
+
+  console.log('lightTheme', lightTheme)
+
+  return (
+    <ThemeProvider theme={lightTheme ? LIGHT_THEME : DARK_THEME}>
+      <HelloWorld />
+      <div>
+        <button type="button" onClick={switchThemeHandler}>
+          Change theme
+        </button>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
